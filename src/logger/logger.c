dd674f690fce68d5edbd0fa0a2ee7d01fa0b266f
Tom SCHMITT <tom.schmitt@etu.uca.fr>
2026-08-15 18:24:34 +0200

    Modifying logger format

--- src/logger/logger.c
+++ src/logger/logger.c
@@ -124,7 +124,7 @@ uint8_t logMsg(const uint8_t code, const char* modname, const char* msg)
     return 1;
 
   #ifdef LOGFILE
-  fprintf(OUT, "%Lf %s %s %s\n", getRunTime(), modname, LEVEL[code], msg);
+  fprintf(OUT, "%Lf | %s | %s %s\n", getRunTime(), modname, LEVEL[code], msg);
   #else
   fprintf(OUT, "%Lf | %s | %s%s\e[0m - %s\n", getRunTime(), modname, COLOR_LEVEL[code], LEVEL[code], msg);
   #endif
