f3a3a3b2381a5339e4088cdf8e7768fe86d13fb6
Tom SCHMITT <tom.schmitt@etu.uca.fr>
2026-08-26 01:28:28 +0200

    Cleaning dirty code

--- src/net/protocol.c
+++ src/net/protocol.c
@@ -1,31 +1,36 @@
 #include "protocol.h"
 
-enum PROTOCOL_OPCODE {
-  HANDSHAKE,
-  PEER_RENDEZVOUS,    // Open new connection for current peer
-  FOREIGN_RENDEZVOUS, // Ask to be contacted by other peer
-  KEEPALIVE,
-  PING,
-  PONG,
-  DATA,
-  DISCONNECT
+enum OPCODE {
+  OP_HANDSHAKE,
+  OP_PEER_RENDEZVOUS,    // Open new connection for current peer
+  OP_FOREIGN_RENDEZVOUS, // Ask to be contacted by other peer
+  OP_KEEPALIVE,
+  OP_NODE_DISCOVERY, // Initially used + gossip each delta t
+  OP_PING,
+  OP_PONG,
+  OP_DATA,
+  OP_DISCONNECT
 };
 
-enum PROTOCOL_MSG_TYPE {
-  NODE_DISCOVERY,
+typedef enum opcode_t;
+
+enum MSG_TYPE {
   PARANOIA_ANTIHACK,
   MSG_DATA,
   MUSIC_DATA,
   AUDIO_DATA,
   VIDEO_DATA
-}
+};
 
+typedef enum msg_type_t;
 
 struct P2P_PACKET {
-  PROTOCOL_OPCODE   opcode;
-  uint32_t        data_len;
-  uint8_t*            data;
-}
+  op_code_t          opcode;
+  uint32_t         data_len;
+  uint8_t            data[];
+};
+
+typedef struct P2P_PACKET p2p_packet_t;
 
 int protocol_handshake(void) {
   return 0;
