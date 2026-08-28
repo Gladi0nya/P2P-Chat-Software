2a1e63dbb679d54697c1a524bdb994eb101dd5c4
gh608 <baraug100+github@gmail.com>
2026-08-28 17:27:43 +0200

    Change from malloc to buffer actions for log_date_and_time

--- src/logger/logger.c
+++ src/logger/logger.c
@@ -12,127 +12,127 @@
 
 #include "logger.h"
 #include "generic_type.h"
-// #include "string_acts.h" Not used so I removed it
+
+#ifdef LOG_USE_DATETIME
+  #include "string_acts.h"
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <time.h>
 
+
+//DEBUG_MOD         : Debug mode is declared as turned off
+
+//LOG_NO_TERMINAL   : Not logging inside of the terminal
+
+//LOG_USE_DATETIME  : Choosing the type of time used (date&time OR time from the begin of the program)
+//                                                    defined   OR      not defined
+
+//LOG_NAME_FILE     : Choosing the name of the log file
+
+
 // Is logger initialized ? (1 = yes, 0 = not)
 static uint8_t is_init = 0;
 
 #ifdef LOG_USE_FILE
     // File used to make logs
-    static FILE* restrict OUT = NULL;
+    static FILE* restrict LOG_OUTPUT = NULL;
 #else
-    #ifdef LOG_USE_AUGUSTIN
-        #undef LOG_USE_AUGUSTIN
+    #ifdef LOG_NO_TERMINAL
+        #undef LOG_NO_TERMINAL
     #endif
 #endif
 
 #ifdef LOG_USE_DATETIME
-// Size that should be used to format the time if using date and time type
-static uint8_t size_time = 0;
+  // Size that should be used to format the time if using date and time type
+  static uint8_t size_time = 0;
 
-/** ------------------------------------------------------------------------------------------- *
- *  date_and_time                                                                               *
- *                                                                                              *
- *  Retrieve and format the current date and time into a string                                 *
- *                                                                                              *
- *  @param formated     pointer of the char* for the formated time has to no be allocated       *
- *  @param on_terminal  boolean to know whether the time will be printed on terminal            *
- *                      0 = not on terminal                                                     *
- *                                                                                              *
- *  @retval 0   pointer foration was done well                                                  *
- *  @retval 1   could not get the time                                                          *
- *  @retval 2   memory allocation failed                                                        *
- *  ------------------------------------------------------------------------------------------- **/
+  /** ------------------------------------------------------------------------------------------- *
+   *  log_date_and_time                                                                           *
+   *                                                                                              *
+   *  Retrieve and format the current date and time into a string                                 *
+   *                                                                                              *
+   *  @param formated     pointer of the char* for the formated time has to no be allocated       *
+   *                      the length of the buffer has to be at least 20 (until year 9999)        *
+   *  @param on_terminal  boolean to know whether the time will be printed on terminal            *
+   *                      0 = not on terminal, 1 = on terminal                                    *
+   *                                                                                              *
+   *  @retval 0   pointer foration was done well                                                  *
+   *  @retval 1   could not get the time                                                          *
+   *  @retval 2   memory allocation failed                                                        *
+   *  ------------------------------------------------------------------------------------------- **/
 
-uint8_t date_and_time(char** restrict formated, const uint16_t on_terminal) // TODO: This function looks kinda fatty, need of reducing its size and optimizing it.
-{
-  uint8_t index = 0;
-  time_t now = time(NULL);
-  struct tm* time = localtime(&now);
-  
-  if (time == NULL)
-    return 1;
-  
-  char* converted[7] = {
-    int_to_char((int32_t)time->tm_mday),
-    int_to_char((int32_t)time->tm_mon + 1),
-    int_to_char((int32_t)(time->tm_year + 1900)),
-    int_to_char((int32_t)time->tm_hour),
-    int_to_char((int32_t)time->tm_min),
-    int_to_char((int32_t)time->tm_sec),
-    NULL
-  };
-  
-  if (size_time == 0)
-    size_time = 2 + 2 + length(converted[2]) + 2 + 2 + 2 + 1 * 6;
-  
-  
-  (*formated) = malloc(sizeof(char) * (size_time + 1));
-  
-  if ((*formated) == NULL) {
-
-    for (uint8_t k = 0 ; converted[k] != NULL ; k++)
-      free(converted[k]);
-    
-    return 2;
-  }
-  
-  for (uint8_t index_converted = 0 ; converted[index_converted] != NULL ; index_converted++) {
-
-    if (converted[index_converted] == NULL) {
-
-      for (int k = 0 ; converted[k] != NULL ; k++)
-	free(converted[k]);
-          
-      free(*formated);
-
-      return 2;
-
-    } else if (converted[index_converted][1] == '\0') {
-      
-      (*formated)[index] = '0';
-      (*formated)[index + 1] = converted[index_converted][0];
-
-      index += 2;
-      
-    } else {
-
-      for (uint32_t index_string = 0 ; converted[index_converted][index_string] != '\0' ; index_string++) {
-	(*formated)[index] = converted[index_converted][index_string];
-	
-	index++;
+  uint8_t log_date_and_time(char* restrict const formated,const uint16_t on_terminal)
+  {
+      time_t now = time(NULL);
+      struct tm* time = localtime(&now);
+      if (time == NULL) {
+          return 1;
       }
-    }
+      char* converted[7] = {
+          int_to_char((int32_t)time->tm_mday),
+          int_to_char((int32_t)time->tm_mon + 1),
+          int_to_char((int32_t)(time->tm_year + 1900)),
+          int_to_char((int32_t)time->tm_hour),
+          int_to_char((int32_t)time->tm_min),
+          int_to_char((int32_t)time->tm_sec),
+          NULL
+      };
+      uint8_t index = 0;
+      if (size_time == 0) {
+          size_time = 2 + 2 + length(converted[2]) + 2 + 2 + 2 + 1 * 6;
+      }
+      formated[size_time] = '\0';
+      
+      for (uint8_t index_converted = 0 ; converted[index_converted] != NULL ; index_converted++) {
+          
+          if (converted[index_converted] == NULL) {
+              for (int k = 0 ; converted[k] != NULL ; k++) {
+                  free(converted[k]);
+              }
 
-    switch(index_converted) {
-        case 1:
-	  (*formated)[index] = '/';
-	  break;
-        case 4:
-	  (*formated)[index] = ':';
-	  break;
-        default:
-	  if (on_terminal) {
-	    (*formated)[index] = ' ';
-	  } else {
-	    (*formated)[index] = '\t';
-	  }
-    }
+              return 2;
 
-    free(converted[index_converted]);
+          } else if (converted[index_converted][1] == '\0') {
+              formated[index] = '0';
+              formated[index + 1] = converted[index_converted][0];
 
-    index++;
+              index += 2;
+
+          } else {
+              for (uint32_t index_string = 0 ; converted[index_converted][index_string] != '\0' ; index_string++) {
+                  formated[index] = converted[index_converted][index_string];
+
+                  index++;
+              }
+          }
+
+          switch(index_converted) {
+              case 0:
+              case 1:
+                  formated[index] = '/';
+                  break;
+              
+              case 3:
+              case 4:
+                  formated[index] = ':';
+                  break;
+
+              default:
+                  if (on_terminal) {
+                      formated[index] = ' ';
+                  } else {
+                      formated[index] = '\t';
+                  }
+          }
+          free(converted[index_converted]);
+          index++;
+      }
+      return 0;
   }
 
-  (*formated)[size_time - 1] = '\0';
-
-  return 0;
-}
 #else
 // Time of the begin of the program
 static struct timespec start_time = {0};
@@ -150,12 +150,12 @@ static inline uint8_t log_time_spent(float80_t* elapsed)
 {
   struct timespec current;
   
-  if (clock_gettime(CLOCK_MONOTONIC, &current) == -1)
+  if (clock_gettime(CLOCK_MONOTONIC, &current) == -1) {
     return 1;
-  
+  }
 
   *elapsed = (current.tv_sec - start_time.tv_sec) +
-    (current.tv_nsec - start_time.tv_nsec) / 1000000000.0L;
+             (current.tv_nsec - start_time.tv_nsec) / 1000000000.0L;
 
   return 0;
 }
@@ -178,10 +178,10 @@ uint8_t log_init(void)
     return 2;
 
   #ifdef LOG_USE_FILE
-  if (OUT != NULL)
+  if (LOG_OUTPUT != NULL)
     return 1;
   
-  if ((OUT = fopen(NAME_FILE, "a+")) != NULL)
+  if ((LOG_OUTPUT = fopen(NAME_FILE, "a+")) != NULL)
     return 1;
   #endif
 
@@ -192,12 +192,12 @@ uint8_t log_init(void)
         
   #ifdef DEBUG
 
-  #ifndef LOG_USE_AUGUSTIN
+  #ifndef LOG_NO_TERMINAL
   fprintf(stdout, "Log initialized.\n");
   #endif
   
   #ifdef LOG_USE_FILE
-  fprintf(OUT, "\tLog initialized.\n");
+  fprintf(LOG_OUTPUT, "\tLog initialized.\n");
   #endif
 
   #endif
@@ -225,25 +225,25 @@ uint8_t log_shutdown(void) {
 
   #ifdef DEBUG
 
-  #ifndef LOG_USE_AUGUSTIN
+  #ifndef LOG_NO_TERMINAL
   fprintf(stdout, "Log stopped.\n");
   #endif
   
   #ifdef LOG_USE_FILE
-  fprintf(OUT, "\tLog stopped.\n");
+  fprintf(LOG_OUTPUT, "\tLog stopped.\n");
   #endif
 
   #endif
 
   #ifdef LOG_USE_FILE
-  if (OUT != NULL) {
-    if (fclose(OUT)){
+  if (LOG_OUTPUT != NULL) {
+    if (fclose(LOG_OUTPUT)){
       LOG_ERROR("Failed to close the log file");
 
       return 1;
     }
 		
-    OUT = NULL;
+    LOG_OUTPUT = NULL;
   }
   #endif
 
@@ -286,7 +286,7 @@ uint8_t log_write(const uint8_t level, const char* restrict const filename,
     "DEBUG"
   };
 
-  #ifndef LOG_USE_AUGUSTIN
+  #ifndef LOG_NO_TERMINAL
   const char* const restrict LEVEL_COLOR[] = {
     "\033[0;31m",
     "\033[0;33m",
@@ -296,18 +296,17 @@ uint8_t log_write(const uint8_t level, const char* restrict const filename,
   #endif
 
   #ifdef LOG_USE_DATETIME
-  char* const restrict current_time;
+  char current_time[25];
 
-  #ifndef LOG_USE_AUGUSTIN
-  if (!(date_and_time((char** restrict)&current_time,1)))
+  #ifndef LOG_NO_TERMINAL
+  if (!(date_and_time(current_time,1)))
     fprintf(stdout,"%s | %s%s\033[0m | %s [%s:%s:%li]\n", current_time, LEVEL_COLOR[level], LEVEL_MSG[level], msg, filename, funcname, line);
 
   free(current_time);
   #endif
 
   #ifdef LOG_USE_FILE
-  if ((date_and_time((char** restrict)&current_time,0))) {
-
+  if (!(date_and_time(current_time,0))) {
     fprintf(LOGFILE,"%s\t%s\t%s\t%s\t%s\t%li\n", current_time, LEVEL_MSG[level], msg, filename, funcname, line);
     fflush(LOGFILE);
   }
@@ -323,7 +322,7 @@ uint8_t log_write(const uint8_t level, const char* restrict const filename,
     return 1;
 
 	    
-  #ifndef LOG_USE_AUGUSTIN
+  #ifndef LOG_NO_TERMINAL
 
   #ifdef __x86_64__
   fprintf(stdout,"%015.9Lf | %s%s\033[0m | %s [%s:%s:%li]\n", elapsed, LEVEL_COLOR[level], LEVEL_MSG[level], msg, filename, funcname, line);
