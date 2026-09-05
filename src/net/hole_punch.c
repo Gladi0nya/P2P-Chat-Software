11f3ea84a551c1431e5ad5339f4ca3c76b87cbf9
Tom SCHMITT <tom.schmitt@etu.uca.fr>
2026-09-05 16:44:09 +0200

    Test

--- src/net/hole_punch.c
+++ src/net/hole_punch.c
@@ -2,6 +2,8 @@
 
 #include "logger/logger.h"
 
+
+#include <arpa/inet.h>
 #include <stdio.h>
 
 struct __attribute__((packed)) PUNCH_PACKET {
@@ -18,6 +20,14 @@ int hole_punch_send_one(peer_context_t *ctx, opcode_t opcode)
     .id = ctx->packet_id++
   };
 
+  char ip_str[INET_ADDRSTRLEN];
+  inet_ntop(AF_INET, &ctx->peer_addr.sin_addr, ip_str, sizeof(ip_str));
+  LOG_DEBUG("Sending to %s:%d (opcode=0x%02X, id=%llu)",
+	    ip_str,
+	    ntohs(ctx->peer_addr.sin_port),
+	    packet.op,
+	    (unsigned long long)packet.id);
+  
   LOG_DEBUG("Sending packet: op=0x%02X, id=%llu, size=%zu",
 	    packet.op, (unsigned long long)packet.id, sizeof(packet));
 
