b2d9e83bf892c832f6ab2bca8647b95e2144f555
Tom SCHMITT <tom.schmitt@etu.uca.fr>
2026-09-05 16:31:57 +0200

    Trying to fix  bug on AND EPyc Milan

--- src/net/hole_punch.c
+++ src/net/hole_punch.c
@@ -2,6 +2,8 @@
 
 #include "logger/logger.h"
 
+#include <stdio.h>
+
 struct __attribute__((packed)) PUNCH_PACKET {
   opcode_t op;
   uint64_t id;
@@ -15,6 +17,17 @@ int hole_punch_send_one(peer_context_t *ctx, opcode_t opcode)
     .op = opcode,
     .id = ctx->packet_id++
   };
+
+  LOG_DEBUG("Sending packet: op=0x%02X, id=%llu, size=%zu",
+	    packet.op, (unsigned long long)packet.id, sizeof(packet));
+
+  unsigned char *bytes = (unsigned char*)&packet;
+  printf("Hex dump: ");
+  for (size_t i = 0; i < sizeof(packet); i++) {
+    printf("%02X ", bytes[i]);
+  }
+  printf("\n");
+  fflush(stdout);
   
   ssize_t sent = sendto(ctx->sock, &packet, sizeof(packet), 0, (struct sockaddr*)&ctx->peer_addr, sizeof(ctx->peer_addr));
 
